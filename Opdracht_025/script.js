a8f0b394fd94281cdfec6d92d711af96d7970790
TalithadeKramer <76871148+TalithadeKramer@users.noreply.github.com>
2021-03-04 13:56:06 +0100

    Add files via upload

---
+++ Opdracht_025/script.js
@@ -0,0 +1,23 @@
+// function add(number1, number2) {
+//     const sum = number1 * number2;
+//     return sum;
+// };
+
+// const result = add(3, 3);
+// console.log(result);
+
+// const add = function (number1, number2) {
+//     const sum = number1 * number2;
+//     return sum;
+// };
+
+// const result = add(2, 2);
+// console.log(result);
+
+const add = (number1, number2) => {
+    const sum = number1 * number2;
+    return sum;
+};
+
+const result = add(4, 4);
+console.log(result);
